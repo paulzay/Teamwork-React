2bc044219d2a4b6e76f3f94a10747b0c38034469
paulzay <paulotieno2@gmail.com>
2020-04-20 18:43:23 +0300

    Environment build and initial app structure.
    Installed various dependencies, configured webpack and eslint and set up the initial app structure.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="container-fluid">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+
+
+render(
+    <Router>
+      <App />
+    </Router>
+  ,
+  document.getElementById("app")
+);
