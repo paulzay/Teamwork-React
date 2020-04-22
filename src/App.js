1b43a0f5581f7cf2bfc509b42ce5847fe377c66a
paulzay <paulotieno2@gmail.com>
2020-04-22 10:07:02 +0300

    Make Home/landing page component
    Used jumbotron from bootstrap for the home page. Might change it later
    so as to avoid the webapp becoming too generic. it will do for now.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navigation from "./components/layout/Navigation";
-
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div>
       <Navigation />
-      
+      <Home />
     </div>
   );
 }
